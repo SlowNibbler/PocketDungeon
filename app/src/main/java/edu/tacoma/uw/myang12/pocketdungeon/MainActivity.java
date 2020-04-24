377520745b90447a96f1d37aee2372667073a66c
Meng Yang <yobank@gmail.com>
2020-04-24 13:52:11 -0700

    Added main page and register page

--- app/src/main/java/edu/tacoma/uw/myang12/pocketdungeon/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/myang12/pocketdungeon/MainActivity.java
@@ -2,7 +2,9 @@ package edu.tacoma.uw.myang12.pocketdungeon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when user taps the "Create a new account" button */
+    public void openRegisterActivity(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
